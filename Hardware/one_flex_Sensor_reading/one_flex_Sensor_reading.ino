d7ce12478e7bf79954ea9dcf7209d3d8cac2858c
Semanti-Space <semantidatta24@gmail.com>
2026-07-12 10:12:37 +0530

    First flex sensor threshold

--- Hardware/one_flex_Sensor_reading/one_flex_Sensor_reading.ino
+++ Hardware/one_flex_Sensor_reading/one_flex_Sensor_reading.ino
@@ -8,14 +8,24 @@ void setup()
 void loop() 
 {
   int reader = 0;
-  int c = 0;
+  long c = 0;
   int average = 0;
-  for (int i = 1; i<=10; i++)
+  for (int i = 1; i<=20; i++)
   {
     reader = analogRead(34);
     c = c + reader;
+    delay(2);
   }
-  average = c/10;
+  average = c/20;
   Serial.print(" Reading of Pin34 "); Serial.println(average);
-  delay(100);
+  if (average>3200)
+  {
+    Serial.print(" Finger is  "); Serial.println("BENT");
+  }
+  if (average< 3200)
+  {
+    Serial.print(" Finger is "); Serial.println("STRAIGHT");
+  }
+  
+  delay(300);
 }
