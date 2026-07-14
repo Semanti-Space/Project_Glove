8bb04cee994421e6696e1e82485f26b1cdd9c863
Semanti-Space <semantidatta24@gmail.com>
2026-07-15 02:36:53 +0530

    First 3 flex sensors working

--- Hardware/one_flex_Sensor_reading/one_flex_Sensor_reading.ino
+++ Hardware/one_flex_Sensor_reading/one_flex_Sensor_reading.ino
@@ -10,6 +10,12 @@ void loop()
   int reader = 0;
   long c = 0;
   int average = 0;
+  int reader2 = 0;
+  long c2 = 0;
+  int avg2 = 0;
+  int reader3 = 0;
+  long c3 = 0;
+  int avg3 = 0;
   for (int i = 1; i<=20; i++)
   {
     reader = analogRead(34);
@@ -26,6 +32,29 @@ void loop()
   {
     Serial.print(" Finger is "); Serial.println("STRAIGHT");
   }
-  
+  for (int j = 1; j<=20; j++)
+  {
+    reader2 = analogRead(32);
+    c2 = c2 + reader2;
+    delay(2);
+  }
+  avg2 = c2/20;
+  Serial.print(" Reading of Pin32 "); Serial.println(avg2);
+  if (avg2>1400)
+  {
+    Serial.print(" Finger is  "); Serial.println("BENT");
+  }
+  if (avg2<1200)
+  {
+    Serial.print(" Finger is "); Serial.println("STRAIGHT");
+  }
+  for (int k = 1; k<=20; k++)
+  {
+    reader3 = analogRead(35);
+    c3 = c3 + reader3;
+    delay(2);
+  }
+  avg3 = c3/20;
+  Serial.print(" Reading of Pin35 "); Serial.println(avg3);
   delay(300);
 }
